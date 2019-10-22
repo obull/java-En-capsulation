1dd1c3ae28d1d4e7cfe63a845e9d62dc8ece7f38
Rihanna <Rihanna@DESKTOP-6QQF9JU.fios-router.home>
2019-10-22 16:07:31 -0400

    first

---
+++ En capsulation/src/EncapVerible.java
@@ -0,0 +1,13 @@
+
+public class EncapVerible {
+	/* File name : RunEncap.java */
+
+	public static void main(String args[]) {
+		EncapTest encap = new EncapTest();
+		encap.setName("James");
+		encap.setAge(20);
+		encap.setIdNum("12343ms");
+
+		System.out.print("Name : " + encap.getName() + " Age : " + encap.getAge());
+	}
+}
